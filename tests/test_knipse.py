729fceb8eb064396b0132fd27c6ffe7a76298948
luphord <luphord@protonmail.com>
2020-03-21 07:26:00 +0100

    initial commit based on cookiecutter

---
+++ tests/test_knipse.py
@@ -0,0 +1,12 @@
+import unittest
+
+from knipse import parser
+
+
+class Testknipse(unittest.TestCase):
+
+    def test_argument_parsing(self):
+        args = parser.parse_args([])
+        self.assertEqual(args.version, False)
+        args = parser.parse_args(['--version'])
+        self.assertEqual(args.version, True)
